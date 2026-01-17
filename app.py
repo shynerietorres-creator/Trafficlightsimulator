4fb63666f0658747c7331e2acf9789fa638b6a65
shynerietorres-creator <shynerietorres@gmail.com>
2026-01-17 13:01:13 +0800

    app.py

--- app.py
+++ app.py
@@ -44,5 +44,6 @@ if __name__ == "__main__":
     t.daemon = True
     t.start()
 
-    port = int(os.environ.get("PORT, 5000"))
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
